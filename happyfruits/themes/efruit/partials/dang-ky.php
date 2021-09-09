9492df56165c21bc7ec31a510b6bb3d2264c5d7b
VoDangVinh <01686228222vinh@gmail.com>
2021-09-09 09:38:25 +0700

    update note notice register password

--- happyfruits/themes/efruit/partials/dang-ky.php
+++ happyfruits/themes/efruit/partials/dang-ky.php
@@ -28,10 +28,10 @@
 
                             <div class="password-wrapper">
 
-                                <input pattern="(?=.*\d)(?=.*[A-Za-z])(?!.*\s).{8,}" title="Password must have limited one number, one alphabetic characters and minimum length is 8" autocomplete="on" id="password" type="password" name="password_en" class="input-account" autocapitalize="words" placeholder="Password" required>
+                                <input pattern="(?=.*\d)(?=.*[A-Za-z])(?!.*\s).{8,}" title="Password must have limited one number, one alphabetic characters and minimum length is 5" autocomplete="on" id="password" type="password" name="password_en" class="input-account" autocapitalize="words" placeholder="Password" required>
                                 <i class="fas fa-eye" id="btnShowPass"></i>
                             </div>
-                            <p class="efruit-en">Password must have limited one number and at least 5 characters.</p>
+                            <p class="efruit-en">Password must have limited one number, one alphabetic characters and minimum length is 5</p>
                             <p class="efruit-vi">Mật khẩu phải có một số, một kí tự chữ cái và tối thiểu 5 kí tự</p>
                             <input id="phone" type="number" name="phone_en" class="input-account" autocapitalize="words" placeholder="Phone" required>
                                 <!--aaa!-->
